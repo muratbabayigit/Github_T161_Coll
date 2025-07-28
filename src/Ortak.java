23f08c38fd92e2ba08e18b09defb24d18b49d5c8
tunahan <tnh_cevik@gmail.com>
2025-07-28 21:51:41 +0300

    v2.1 tunahan

--- src/Ortak.java
+++ src/Ortak.java
@@ -1,7 +1,7 @@
 public class Ortak {
     public static void main(String[] args) {
         System.out.println("buradan bir babayigit geçti");
-        System.out.println("buralar karışacak vaziyet alın");
+        System.out.println("buralar yeşillendi");
 
 
 
