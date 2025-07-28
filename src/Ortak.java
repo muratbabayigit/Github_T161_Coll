36c2ba7a0404be28e9400cbfef2690b207e25852
Onur <onr.koksal@hotmail.com>
2025-07-28 21:47:55 +0300

    v2 1 Onur

--- src/Ortak.java
+++ src/Ortak.java
@@ -1,5 +1,8 @@
 public class Ortak {
     public static void main(String[] args) {
         System.out.println("buradan bir babayigit geçti");
+        System.out.println("buralar karışıcak dagılın");
     }
+
+
 }
